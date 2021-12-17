b8290682f0cb707db090f630a7fe30b43dc03609
Liam Cole <liam@liamiam.com>
2021-12-16 16:57:39 -0800

    Initial commit

---
+++ Day13/Program.cs
@@ -0,0 +1,24 @@
+﻿if (args.Length < 1) throw new InvalidOperationException("need single argument containing file path to input");
+
+List<Point> points; // `Point` at bottom
+
+foreach (var line in File.ReadLines(args[0]))
+{
+    
+}
+
+struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+}
+
+interface IFold
+{
+    void Perform(ref List<Point> points);
+}
+
+struct XFold
+{
+    public int Value { get; init; }
+}
